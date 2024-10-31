d047572b1f2cc8e09c2c06b6ed5d668db7d87d33
Azimwer <153096293+Azimwer@users.noreply.github.com>
2024-10-31 17:58:05 +0500

    Add files via upload

---
+++ switchJS 2/main.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.buy-button').forEach(button => {
+    button.addEventListener('click', function() {
+        let input = prompt("Сколько сетов хотите заказать?");
+        let number = Number(input);
+
+        // Используем конструкцию switch (true) для проверки условия
+        switch (true) {
+            case number > 0 && input !== "":
+                alert(`Ваш заказ в количестве ${number} был принят`);
+                alert(`Стоимость вашего заказа: ${number * 45000} сум`); // Второй alert с суммой
+                break;
+
+            default:
+                alert("Пожалуйста, введите корректное число");
+                break;
+        }
+    });
+});
